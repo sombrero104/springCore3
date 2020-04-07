953871c76d1332b7f35c4990445fcecd1100dd7a
rey <glengleann@gmail.com>
2020-04-07 16:25:04 +0900

    .

--- src/main/java/sombrero/aop/spring_aop/PerfAspect.java
+++ src/main/java/sombrero/aop/spring_aop/PerfAspect.java
@@ -7,11 +7,18 @@ import org.springframework.stereotype.Component;
 
 /**
  * @Aspect 애노테이션을 붙여서 Aspect 역할을 하는 클래스라는 것을 알려주도록 한다.
+ * 빈으로 등록해야 하니까 (컴포넌트 스캔을 사용한다면) @Component도 추가.
  */
 @Component
 @Aspect
 public class PerfAspect {
 
+    /**
+     * 아래에서 인자로 받는 ProceedingJoinPoint 는 => 어드바이스가 적용되는 대상.
+     * (이 예제에서는 SimpleEventService2 클래스의 createEvent(), publishEvent() 메서드 자체라고 보면 된다.)
+     * 메서드 invocation과 비슷한 개념.
+     * 메서드 실행 => pjp.proceed();
+     */
     @Around("execution(* sombrero.aop.spring_aop.EventService.*(..))")
     public Object logPerf(ProceedingJoinPoint pjp) throws Throwable {
         long begin = System.currentTimeMillis();
